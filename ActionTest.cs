8f98a3be77651e3d84d958c7ddcc8d455ba9771c
Mixdogg <jjm6564@naver.com>
2022-01-18 17:00:23 +0900

    Prefab Instantiate

--- ActionTest.cs
+++ ActionTest.cs
@@ -4,25 +4,43 @@ using UnityEngine;
 using Valve.VR;
 public class ActionTest : MonoBehaviour
 {
+    public Object leftController;
     public SteamVR_Input_Sources handType;
     public SteamVR_Action_Boolean menu;
     public SteamVR_Action_Boolean grab;
+    public GameObject canvas;
 
-    public bool GetMenuDown(){
-        return menu.GetStateDown(handType);
+    public bool GetMenu(){
+        return menu.GetState(handType);
     }
     public bool GetGribDown(){
         return grab.GetStateDown(handType);
     }
+ 
+    void Awake()
+    {
+        //Instantiate (canvas,new Vector3(0,0,3),Quaternion.Euler(-90f,0f,0f));
+        Instantiate(canvas,new Vector3(0,0,2),canvas.transform.rotation);
+
+    }
+    void Start()
+    {
+        canvas.SetActive(true);
+
+    }
     void Update()
     {
-        if(GetMenuDown()){
+        if(GetMenu()){
             print("ok"+handType);
-            //Debug.Log("ok");
+           // canvas.SetActive(GetMenu());
         }
+        // else{
+        //     canvas.SetActive(GetMenu());
+        // }
          if(GetGribDown()){
             print("grib"+handType);
             //Debug.Log("ok");
         }
+        
     }
 }
